f24bcad508b636b5d1f788d9c49c94fdae113938
Michamoro <mimoro1176@hotmail.com>
2023-06-27 17:56:16 -0500

    Springfox implementation (spring boot 3 not supported)

--- SpringCache/src/main/java/dev/SpringCache/controller/CustomerController.java
+++ SpringCache/src/main/java/dev/SpringCache/controller/CustomerController.java
@@ -3,6 +3,7 @@ package dev.SpringCache.controller;
 import dev.SpringCache.dto.CustomerDTO;
 import dev.SpringCache.service.CustomerService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +18,13 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.OK)
     public ResponseEntity<List<CustomerDTO>> all() {
         return ResponseEntity.ok(customerService.all());
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.CREATED)
     public ResponseEntity<CustomerDTO> save(@RequestBody CustomerDTO request) {
         customerService.save(request);
         return ResponseEntity.ok().build();
