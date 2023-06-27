2bbecc52fb8054baee9e253f4d230bd586024e8c
Michamoro <mimoro1176@hotmail.com>
2023-06-26 19:31:50 -0500

    initial commit

---
+++ SpringCache/database/springcache.sql
@@ -0,0 +1,8 @@
+create database springcache;
+
+use springcache;
+
+create table customers (
+  customer_id int auto_increment primary key,
+  customer_name varchar(200) not null
+);
---
+++ SpringCache/src/test/java/dev/SpringCache/SpringCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.SpringCache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
